73d9637258ed0851f47f58c8680380601ca2d44e
ibrahimrimi <khalilrimi393@gmail.com>
2025-07-20 22:47:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  const menu = document.getElementById("nav-menu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+});
